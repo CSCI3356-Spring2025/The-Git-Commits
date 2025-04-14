8e7eda11b096b643032624b249f5841e11092b21
Caden Parajuli <caden.parajuli@member.fsf.org>
2025-04-14 04:21:41 -0400

    feat: asssessment title and date editing ability

--- assessments/urls.py
+++ assessments/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path('assessment_list/', views.StudentAssessmentListView.as_view(), name='student_assessment_list'),
     path('assessment_creation/', views.CreateAssessmentView.as_view(), name='assessment_creation'),
 
+    # Why is some of this here when there's already a courses view in the landing app?
+    # Most of it tries to render a template that doesn't exist, or one that's
+    # already in the landing app
     path('professor/courses/', views.ProfessorCoursesView.as_view(), name='professor_courses'),
     path('professor/courses/<int:course_id>/assessments/', views.ProfessorAssessmentsView.as_view(), name='professor_assessments'),
     path('professor/courses/<int:course_id>/assessments/<int:assessment_id>/teams/', views.ProfessorTeamsView.as_view(), name='professor_teams'),
