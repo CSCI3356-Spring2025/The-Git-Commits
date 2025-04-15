39cd6c2ec48f053fbd302613f763d619aa9f51ee
Daven Pelaez <pelaezd@bc.edu>
2025-04-14 23:02:36 -0400

    adding comments for work todo in landing/urls.py

--- landing/urls.py
+++ landing/urls.py
@@ -14,8 +14,13 @@ urlpatterns = [
     path('professor/feedback/', ProfessorFeedbackCoursesView.as_view(), name='professor_feedback_courses'),
     path('professor/feedback/<int:course_id>/', ProfessorFeedbackAssessmentsView.as_view(), name='professor_feedback_assessments'),
     path('professor/feedback/<int:course_id>/<int:assessment_id>/', ProfessorFeedbackTeamsView.as_view(), name='professor_feedback_teams'),
-    path('professor/feedback/<int:course_id>/<int:assessment_id>/<int:team_id>/feedback/', ProfessorIndividualFeedbackView.as_view(), name='professor_feedback_individual'),
-    
+    path('professor/feedback/<int:course_id>/<int:assessment_id>/<int:team_id>/', ProfessorIndividualFeedbackView.as_view(), name='professor_feedback_individual'),
+    #in 'professor_feedback_individual' we still need to create a link from a person's name to the corresponding feedback 
+    # - currently any team is shown to have no members 
+    # - and every user's team and course field contains everyone's courses and teams, not just the individual user's
+    # the url can be something like 'professor/feedback/<int:course_id>/<int:assessment_id>/<int:team_id>/individual_feedback
+
+    #also still have to fix the student feedback flow
     path('student/courses/', StudentCourseListView.as_view(), name='student_courses'),
     path('student/courses/<int:course_id>/assessments/', StudentAssessmentView.as_view(), name='student_assessments'),
     path('student/courses/<int:course_id>/assessments/<int:assessment_id>/feedback/', StudentFeedbackView.as_view(), name='student_feedback'),
